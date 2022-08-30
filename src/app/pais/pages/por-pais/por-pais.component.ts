48105c280f8b6e25e7f4a10835ec8fd05756f1e8
JeanCornelio <jcornelo@isf.do>
2022-08-30 15:46:33 -0400

    Creacion de app de paises

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interfaces';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent {
+
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  
+  constructor( private paisService: PaisService ) { }
+
+  buscar(): void{
+    this.hayError = false;
+    this.paisService.buscarPais( this.termino )
+        .subscribe( ( paises )=>{
+          this.paises = paises;
+          console.log(paises)
+        }, (err)=>{
+          this.hayError = true;
+          this.paises = [];
+        }
+        )
+    
+  }
+
+}
